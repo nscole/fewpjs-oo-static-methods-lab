98fc6cebbe5feee0192cfd773cd0d074c53ca586
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 14:31:06 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ class Formatter {
   static sanitize(word){
     return word.replace(/[^A-Za-z0-9 '-]/g, '');
   };
+  static titleize(string) {
+    let lowerCaseWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let words = string.toLowerCase().split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (!lowerCaseWords.includes(words[i]) || i === 0)
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  }
 }
+

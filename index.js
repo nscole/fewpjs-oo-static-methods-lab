bc28eeaa9844ea0664cb1de7779f9fcc3f5ae25e
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 14:25:43 +1000

    passing 4 test

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ class Formatter {
   //add static methods here
   static capitalize(word){
     return word.charAt(0).toUpperCase() + word.slice(1);
-  }
+  };
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9 '-]/g, '');
+  };
 }
